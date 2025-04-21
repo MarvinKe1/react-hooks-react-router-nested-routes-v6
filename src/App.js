535bd7da26b5562a7d46e443a87a0bf4f0e280a6
Marvin Mango <marvinke2001@gmail.com>
2025-04-21 23:38:00 +0300

    Nested Routing Code-Along

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <header>
-        
+        <NavBar />  {/* This will appear on every page */}
       </header>
+      <main>
+        <Outlet />  {/* Child routes will render here */}
+      </main>
     </>
   );
-};
+}
 
 export default App;
